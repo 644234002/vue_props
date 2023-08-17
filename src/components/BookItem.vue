df5b153097bd0a1a5024d987fbd9112245a98e68
Faqih <644234002@parichat.skru.ac.th>
2023-08-17 12:52:50 +0700

    Props

---
+++ src/components/BookItem.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <b>{{ book.title }}</b><br/>
+        {{ book.shortDescription }}<br/>
+        {{ msg }}
+        <hr/>
+
+    </div>
+</template>
+
+<script setup>
+
+//promp msg
+const props=defineProps(['book','msg'])
+
+const book=props.book
+const msg=props.msg
+//สร้างตัวแปร msg ที่รัค่า props ของ msg
+
+
+</script>
+<style  scoped>
+
+</style>
---
+++ src/components/Books.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <b>Books List:</b>
+
+        <hr/>
+
+        <div v-for="book in props.books">
+            <BookItem :book="book" :msg="msg"/>
+        </div>
+        
+    </div>
+</template>
+
+<script setup>
+import BookItem from './Bookitem.vue'
+
+const props=defineProps(['books','msg'])
+
+
+console.log(props)
+
+</script>
+
+<style  scoped>
+
+</style>
